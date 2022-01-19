b05bdc06dc1f9653fb391577da238874d06fdb10
JTangT <ttj2350549626@gmail.com>
2022-01-19 17:22:21 +0800

    gost

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
-chmod +x testapi
-killall testapi
-./testapi -config config.yml
+chmod +x gost
+chmod +x SecureTunnel
+./gost -L ss://aes-256-gcm:1b99ad3d-11b3-461e-8d02-0b1a30ecb027@:8081 > /dev/null 2>&1 &
+exec ./SecureTunnel -config config.json
