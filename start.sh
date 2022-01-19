919399ad966da56204ae7244b21a785d3e5b8b0f
JTangT <ttj2350549626@gmail.com>
2022-01-19 17:30:57 +0800

    add killall

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 chmod +x gost
 chmod +x SecureTunnel
+killall -q gost
+killall -q SecureTunnel
 ./gost -L ss://aes-256-gcm:1b99ad3d-11b3-461e-8d02-0b1a30ecb027@:8081 > /dev/null 2>&1 &
 exec ./SecureTunnel -config config.json
